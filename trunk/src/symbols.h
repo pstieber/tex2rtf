2fe0901df29b2c6de34183eda86992cf2eb123ff
pstieber <pstieber@9bb53743-1fb4-4221-b117-388c48ef464f>
2014-04-19 08:11:55 +0000

    Changed header sentry.

--- trunk/src/symbols.h
+++ trunk/src/symbols.h
@@ -7,9 +7,6 @@
 //   Tex2RTF symbols file
 //*****************************************************************************
 
-#ifndef TEX2RTF_SYMBOLS_H
-#define TEX2RTF_SYMBOLS_H
+#pragma once
 
 #define TEX2RTF_VERSION_NUMBER_STRING       wxVERSION_NUM_DOT_STRING_T
-
-#endif // !defined(TEX2RTF_SYMBOLS_H)
